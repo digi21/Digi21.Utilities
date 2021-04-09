316c6682f7616fa4f3188ccf0145baa015dd7516
José Ángel <joseangelmt@outlook.com>
2021-04-09 09:16:52 +0200

    Added types

---
+++ Digi21.Utilities/IListExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Digi21.Utilities
+{
+    /// <summary>
+    /// Descargado de https://stackoverflow.com/a/33104162/583336
+    /// </summary>
+    public static class IListExtension
+    {
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            if (list is List<T> asList)
+            {
+                asList.AddRange(items);
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    list.Add(item);
+                }
+            }
+        }
+    }
+}
---
+++ Digi21.Utilities/IReadOnlyDrawingFileExtension.cs
@@ -0,0 +1,10 @@
+﻿using Digi21.DigiNG.Entities;
+using Digi21.DigiNG.IO;
+
+namespace Digi21.Utilities
+{
+    public static class IReadOnlyDrawingFileExtension
+    {
+        public static bool Contains(this IReadOnlyDrawingFile file, Entity entity) => entity.Owner == file;
+    }
+}
