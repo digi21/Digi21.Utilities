1bcebb1e36cbf67ba5552ac9e6b7b5571c5d745e
José Ángel <joseangelmt@outlook.com>
2022-02-03 17:58:14 +0100

    Versión 22.1.0

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,3 +1,3 @@
 @echo off
 nuget.exe pack Digi21.Utilities.nuspec
-nuget.exe sign Digi21.Utilities.22.0.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
+nuget.exe sign Digi21.Utilities.22.1.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
