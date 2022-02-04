57eecc4b2cddffa5578f112a0b88a10e03656df5
José Ángel <joseangelmt@outlook.com>
2022-02-04 10:31:49 +0100

    Revert "Añadido método de extensión IndexOf2D para secuencias de puntos en 3D."
    This reverts commit cd195ec896ff08ff535ae9c98df52c37a59059dc.

--- Digi21.Utilities/UtilidadesSecuenciaPoint3D.cs
+++ Digi21.Utilities/UtilidadesSecuenciaPoint3D.cs
@@ -4,17 +4,8 @@ using Digi21.Math;
 
 namespace Digi21.Utilities
 {
-    /// <summary>
-    /// Proporciona métodos de extensión para secuencias Point3D
-    /// </summary>
     public static class UtilidadesSecuenciaPoint3D
     {
-        /// <summary>
-        /// Indica el índice del punbto pasado por parámetros.
-        /// </summary>
-        /// <param name="secuencia">Secuencia a analizar.</param>
-        /// <param name="punto">Punto a localizar.</param>
-        /// <returns>Índice del punto si éste está en la lista de coordenadas o -1 en caso contrario.</returns>
         public static int IndexOf2D(this IEnumerable<Point3D> secuencia, Point2D punto)
         {
             if (secuencia == null)
@@ -23,7 +14,7 @@ namespace Digi21.Utilities
             var indice = 0;
             foreach (var vertice in secuencia)
             {
-                if (vertice.X.IsEqual(punto.X) && vertice.Y.IsEqual(punto.Y))
+                if (vertice.X == punto.X && vertice.Y == punto.Y)
                     return indice;
                 indice++;
             }
