b93e670a07e5a4b606820305752193d4948c13a0
José Ángel <joseangelmt@outlook.com>
2023-08-22 13:38:59 +0200

    Cambios para compilar para .NET 7

--- Digi21.Utilities/Properties/AssemblyInfo.cs
+++ Digi21.Utilities/Properties/AssemblyInfo.cs
@@ -1,20 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-#if DEBUG
-[assembly: AssemblyTitle("Digi21.UtilitiesD")]
-#else
-[assembly: AssemblyTitle("Digi21.Utilities")]
-#endif
-[assembly: AssemblyDescription("Implementa tipos de utilidad utilizados por componentes de Digi3D")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dreaming With Objects S.L.")]
-[assembly: AssemblyProduct("Digi21.Utilities")]
-[assembly: AssemblyCopyright("Copyright © 2010-2021 José Ángel Martínez Torres")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,19 +12,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d0d28229-5879-4d9e-ac6f-cc494938fb84")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 #else
 [assembly: AssemblyVersion("22.3.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
